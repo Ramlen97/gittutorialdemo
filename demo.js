84ba1fd2c65a9c48727c294893ade5894446bba1
Ramlen97 <rameshwarlenka97@gmail.com>
2022-11-02 17:19:17 +0530

    dompractice1

--- demo.js
+++ demo.js
@@ -1,1 +1,8 @@
-console.log('I love git')
+console.log('I love git');
+const header=document.getElementById('main-header');
+header.style.borderBottom="solid 3px #000";
+
+const addItems=document.querySelector('.title');
+console.log(addItems);
+addItems.style.fontWeight='bold';
+addItems.style.color='green';
