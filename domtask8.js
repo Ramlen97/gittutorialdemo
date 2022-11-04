6963a98d31535aa7a7219e5d864a703f1cbcccb6
Ramlen97 <rameshwarlenka97@gmail.com>
2022-11-04 18:37:10 +0530

    domtask8

--- domtask8.js
+++ domtask8.js
@@ -2,12 +2,35 @@ var form=document.getElementById('addForm');
 var itemList=document.getElementById('items');
 
 form.addEventListener('submit',addItem);
+itemList.addEventListener('click',removeItem);
 
 function addItem(e){
     e.preventDefault();
     var item=document.getElementById('item').value;
+
     var newItem=document.createElement('li');
     newItem.className='list-group-item';
     newItem.append(document.createTextNode(item));
-    console.log(newItem);
+
+    editBtn=document.createElement('button');
+    editBtn.className='btn btn-sm float-right';
+    editBtn.append(document.createTextNode('Edit'));
+    newItem.append(editBtn);
+
+    deleteBtn=document.createElement('button')
+    deleteBtn.className='btn btn-danger btn-sm float-right delete';
+    deleteBtn.append(document.createTextNode('X'));
+    newItem.append(deleteBtn);
+
+    itemList.append(newItem);
 }
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure ?')){
+            var li=e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
